f3f79647d52471d890dab4f9af65941366019a5a
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-24 11:43:42 +0200

    Add project files.

---
+++ Data/PersonajesContext.cs
@@ -0,0 +1,13 @@
+﻿using ApiCrudPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrudPersonajesAWS.Data
+{
+    public class PersonajesContext:DbContext
+    {
+        public PersonajesContext(DbContextOptions<PersonajesContext> options)
+            : base(options) { }
+
+        public DbSet<Personaje> Personajes { get; set; }
+    }
+}
